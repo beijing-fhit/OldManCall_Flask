b4392f8a611561f523001ea73f6a48d9fc976246
licaiMaker <841381242@qq.com>
2020-03-21 20:18:16 +0800

    'update'

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,21 @@
-# 并行工作线程数
+# gunicorn.conf
+
+# 并行工作进程数
 workers = 4
-# 监听内网端口5000【按需要更改】
+# 指定每个工作者的线程数
+threads = 2
+# 监听内网端口5000
 bind = '127.0.0.1:5000'
-# 采用gevent库，支持异步处理请求，提高吞吐量
+# 设置守护进程,将进程交给supervisor管理
+daemon = 'false'
+# 工作模式协程
 worker_class = 'gevent'
-# 设置守护进程【关闭连接时，程序仍在运行】
-daemon = True
-# 设置超时时间120s，默认为30s。按自己的需求进行设置g
-timeout = 120
+# 设置最大并发量
+worker_connections = 2000
+# 设置进程文件目录
+pidfile = '/var/run/gunicorn.pid'
 # 设置访问日志和错误信息日志路径
-# accesslog = './logs/acess.log'
-# errorlog = './logs/error.log'
+accesslog = '/var/log/gunicorn_acess.log'
+errorlog = '/var/log/gunicorn_error.log'
+# 设置日志记录水平
+loglevel = 'warning'
