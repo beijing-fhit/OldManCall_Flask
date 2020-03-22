4f5e0fb6b11e605d23844aa5b5f9d78bc8baaf96
licaiMaker <841381242@qq.com>
2020-03-22 17:09:09 +0800

    update webhook func

--- App/api/v1/WebHookAPI.py
+++ App/api/v1/WebHookAPI.py
@@ -4,7 +4,4 @@ from flask_restful import Resource
 
 class WebHook(Resource):
     def get(self):
-        print("开始部署")
-        print("*"*100)
-        print(request.data)
-        print("部署结束")
+        return  request.data
