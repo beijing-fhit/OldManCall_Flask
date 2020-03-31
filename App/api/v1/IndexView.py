c817428f4f176e933f80b7db5e3857a279876ba3
licaiMaker <841381242@qq.com>
2020-03-31 13:30:38 +0800

    update

--- App/api/v1/IndexView.py
+++ App/api/v1/IndexView.py
@@ -1,11 +1,18 @@
-
+import urllib.parse
 from flask_restful import Resource, url_for
-from flask import redirect
+from flask import redirect, request
+
 
 # from App.ext import api
 
 class Index(Resource):
     def get(self):
+        # 接受
+        params=urllib.parse.urlsplit(request.url).query
+        print(dict(urllib.parse.parse_qs(params)))
+
+        print(dict(request.args))
+
         return redirect(url_for('code'))
 
 
--- App/test.py
+++ App/test.py
@@ -15,5 +15,3 @@
 # print(os.path.abspath(__file__))
 # print(os.path.dirname(os.path.abspath(__file__)))
 # print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
