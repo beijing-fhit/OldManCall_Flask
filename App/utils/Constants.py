e6f27c6123d308a4a25e2462b73b799d6e2fde08
licaiMaker <841381242@qq.com>
2020-04-15 10:58:25 +0800

    update

--- App/utils/Constants.py
+++ App/utils/Constants.py
@@ -1,5 +1,5 @@
 TENCENT_MAP_URL = 'https://apis.map.qq.com/ws/geocoder/v1'
 TENCENT_MAP_KEY = 'ZBLBZ-INS34-TGOUK-DM6IH-LQMVE-QUFU3'
 
-MSG_NOTIFICATION_SEND_URL = 'http://miniapplinux.ucallclub.com/sms/send'
+MSG_NOTIFICATION_SEND_URL = 'https://sendsms.ucallclub.com/sms/send'
 MSG_TEMPLATE_STRING = "【北京峰华】您的验证码是: 1234 "
