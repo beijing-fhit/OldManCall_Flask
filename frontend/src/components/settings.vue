665bac0559b8a58e5518fcb0794d5498fbac0eb4
licaiMaker <841381242@qq.com>
2020-04-22 12:10:13 +0800

    update

--- frontend/src/components/settings.vue
+++ frontend/src/components/settings.vue
@@ -147,10 +147,10 @@ export default {
     // console.log('数据1：', JSON.parse(this.$route.query.getPhoneNumberFromNet))
     var getPhoneNumberFromNet = true
     var temp = this.$route.query.getPhoneNumberFromNet
-    this.$toast('temp:' + (temp === false) + ',sessionStorage:' + sessionStorage.getItem('manInfo'))
     if (temp !== null && temp !== undefined && temp !== '' && temp !== 'undefined') {
       getPhoneNumberFromNet = JSON.parse(temp)
     }
+    this.$toast('getPhoneNumberFromNet:' + (getPhoneNumberFromNet === false) + ',sessionStorage:' + sessionStorage.getItem('manInfo'))
     if (getPhoneNumberFromNet === false) {
       var oldManInfo = this.manInfo
       var temp2 = sessionStorage.getItem('manInfo')
