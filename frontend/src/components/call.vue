4830b1e88ff15bc2cfdc0fd15cfe480d59ee1ce1
licaiMaker <841381242@qq.com>
2020-04-23 18:11:02 +0800

    update

--- frontend/src/components/call.vue
+++ frontend/src/components/call.vue
@@ -50,9 +50,10 @@ export default {
     wx.error(function (res) {
       console.log('error-----')
     })
-    this.$alert('当前地址:' + window.location.href)
+    // this.$alert('当前地址:' + window.location.href)
     api.wxConfig().then(config => {
       console.log('config', config.data)
+      this.$alert('签名:' + config.data.signature)
       wx.config({
         debug: true,
         appId: config.data.appId,
