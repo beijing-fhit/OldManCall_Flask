7341a752b67a408667c431287e8925777283db9c
licaiMaker <841381242@qq.com>
2020-06-14 18:49:15 +0800

    update

--- App/api/v1/ServiceApi.py
+++ App/api/v1/ServiceApi.py
@@ -1,3 +1,4 @@
+
 import time
 
 import requests
@@ -17,7 +18,7 @@ class MsgNotification(Resource):
     def post(self):
         try:
             d = request.data
-            data = json.loads(d)
+            data = json.loads(d,strict=False)
             mobile = data['mobile']
             content = data['content']
             # mobiles = str(mobile).split(',')
@@ -89,110 +90,119 @@ class QRCodeInfo(Resource):
             return res
 
     def post(self):
-        d = request.data
-        data = json.loads(d)
-        qrcodeid = data['qr_code_id']
-        old_man_info = data['old_man_info']
-        phone_numbers = data['phone_number']
-        with open('flask.log', 'a+') as f:
-            f.write('\n[%s] request data is :%s,%s,%s' % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),qrcodeid,old_man_info,phone_numbers))
-            f.close()
-        # print(qrcodeid, old_man_info, phone_numbers)
+        # import sys
+        # from importlib import reload
+        # reload(sys)
+        # sys.setdefaultencoding("utf8")
         try:
-            qrCode = QrCode.query.get(qrcodeid)
-            if qrCode.old_man_info is None or qrCode.old_man_info is '':
-                raise Exception('找不到OldManinfo,oldmaninfo:'+qrCode.old_man_info)
-            # 更新数据
-            db.session.query(OldManInfo).filter(OldManInfo.id == qrCode.old_man_info) \
-                .update({
-                "name": old_man_info['name'],
-                "address": old_man_info['address'],
-                "age": old_man_info['age'] if old_man_info['age'] else 0,
-                "medical_history": old_man_info['medical_history'],
-                "allergy": old_man_info['allergy'],
-                "blood_type": old_man_info['blood_type'],
-                "drugs": old_man_info['drugs'],
-                "treatment": old_man_info['treatment']
-            })
-            db.session.commit()
-            # OldManInfo.query.filter_by(id=qrCode.old_man_info) \
-            #     .update({
-            #     "name":old_man_info['name'],
-            #     "address":old_man_info['address'],
-            #     "age":old_man_info['age'],
-            #     "medical_history":old_man_info['medical_history'],
-            #     "allergy":old_man_info['allergy'],
-            #     "blood_type":old_man_info['blood_type'],
-            #     "drugs":old_man_info['drugs'],
-            #     "treatment":old_man_info['treatment']
-            # })
-
-        except Exception as e:
-            db.session.rollback()  # 回退数据
-            with open('flask.log', 'a+') as f:
-                f.write('\nupdate OldManInfo exception :%s' % (e))
-                f.close()
-            # 创建数据
+            d = request.data
+            data = json.loads(d,strict=False)
+            qrcodeid = data['qr_code_id']
+            old_man_info = data['old_man_info']
+            phone_numbers = data['phone_number']
+            # with open('flask.log', 'a+') as f:
+            #     f.write('\n[%s] request data is :%s,%s,%s' % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),qrcodeid,old_man_info,phone_numbers))
+            #     f.close()
+            # print(qrcodeid, old_man_info, phone_numbers)
             try:
-                # 创建QrCode对象
-                oldManInfo = OldManInfo(name=old_man_info['name'],
-                                        address=old_man_info['address'],
-                                        age=old_man_info['age'] if old_man_info['age'] else 0,
-                                        medical_history=old_man_info['medical_history'],
-                                        allergy=old_man_info['allergy'],
-                                        blood_type=old_man_info['blood_type'],
-                                        drugs=old_man_info['drugs'],
-                                        treatment=old_man_info['treatment'])
-                # print("*"*100)
-                oldManInfo.save()
-                # print("oldManInfo:",oldManInfo)
-                with open('flask.log', 'a+') as f:
-                    f.write('\ncreate OldManInfo info :%s' % (oldManInfo))
-                    f.close()
-
-                if QrCode.query.get(qrcodeid) is  None:
-                    qrCode1 = QrCode(qr_code_id=qrcodeid, old_man_info=oldManInfo.id)
-                    qrCode1.save()
-                else:
-                    qrCode2 = QrCode.query.get(qrcodeid)
-                    qrCode2.old_man_info=oldManInfo.id
-                    qrCode2.save()
+                qrCode = QrCode.query.get(qrcodeid)
+                if qrCode.old_man_info is None or qrCode.old_man_info is '':
+                    raise Exception('找不到OldManinfo,oldmaninfo:'+qrCode.old_man_info)
+                # 更新数据
+                db.session.query(OldManInfo).filter(OldManInfo.id == qrCode.old_man_info) \
+                    .update({
+                    "name": old_man_info['name'],
+                    "address": old_man_info['address'],
+                    "age": old_man_info['age'] if old_man_info['age'] else 0,
+                    "medical_history": old_man_info['medical_history'],
+                    "allergy": old_man_info['allergy'],
+                    "blood_type": old_man_info['blood_type'],
+                    "drugs": old_man_info['drugs'],
+                    "treatment": old_man_info['treatment']
+                })
+                db.session.commit()
+                # OldManInfo.query.filter_by(id=qrCode.old_man_info) \
+                #     .update({
+                #     "name":old_man_info['name'],
+                #     "address":old_man_info['address'],
+                #     "age":old_man_info['age'],
+                #     "medical_history":old_man_info['medical_history'],
+                #     "allergy":old_man_info['allergy'],
+                #     "blood_type":old_man_info['blood_type'],
+                #     "drugs":old_man_info['drugs'],
+                #     "treatment":old_man_info['treatment']
+                # })
 
             except Exception as e:
+                db.session.rollback()  # 回退数据
                 with open('flask.log', 'a+') as f:
-                    f.write('\ncreate OldManInfo exception :%s' % (e))
+                    f.write('\nupdate OldManInfo exception :%s' % (e))
                     f.close()
+                # 创建数据
+                try:
+                    # 创建QrCode对象
+                    oldManInfo = OldManInfo(name=old_man_info['name'],
+                                            address=old_man_info['address'],
+                                            age=old_man_info['age'] if old_man_info['age'] else 0,
+                                            medical_history=old_man_info['medical_history'],
+                                            allergy=old_man_info['allergy'],
+                                            blood_type=old_man_info['blood_type'],
+                                            drugs=old_man_info['drugs'],
+                                            treatment=old_man_info['treatment'])
+                    # print("*"*100)
+                    oldManInfo.save()
+                    # print("oldManInfo:",oldManInfo)
+                    with open('flask.log', 'a+') as f:
+                        f.write('\ncreate OldManInfo info :%s' % (oldManInfo))
+                        f.close()
+
+                    if QrCode.query.get(qrcodeid) is  None:
+                        qrCode1 = QrCode(qr_code_id=qrcodeid, old_man_info=oldManInfo.id)
+                        qrCode1.save()
+                    else:
+                        qrCode2 = QrCode.query.get(qrcodeid)
+                        qrCode2.old_man_info=oldManInfo.id
+                        qrCode2.save()
+
+                except Exception as e:
+                    with open('flask.log', 'a+') as f:
+                        f.write('\ncreate OldManInfo exception :%s' % (e))
+                        f.close()
+                    res = {
+                        'status_code': -1,
+                        'message': '绑定信息失败'
+                    }
+                    return res
+            try:
+                # 创建PhoneNumber对象
+                # phoneNumbers = phone_numbers.split(',')
+                # 对比数据库里的和传进来的电话号码，数据库里多余的要删除，参数里面有不一样的要添加
+                # oldNumbers = qrCode.phone_number # 注意在flask中使用一对多关系，获取到的是一个对象集合，需要获取到具体的值，才能用来做数值比较，否则就是对象之间作比较了，则永远捕不会相等，所以采用下面的写法
+                qrCode = QrCode.query.filter_by(qr_code_id=qrcodeid).first()  # 这重新查询一次
+                oldNumbers = [p.phone_number for p in qrCode.phone_number.all()]
+                delete, add = CommonUtils.compareArrays(oldNumbers, phone_numbers)
+                with open('flask.log', 'a+') as f:
+                    f.write('\ndelete and add phonenumbers :%s,%s' % (delete,add))
+                    f.close()
+                for d in delete:
+                    p1=PhoneNumber.query.filter_by(phone_number=d, q_id=qrcodeid).first()
+                    p1.delete()
+                for a in add:
+                    p2=PhoneNumber(phone_number=a, q_id=qrcodeid)
+                    p2.save()
                 res = {
-                    'status_code': -1,
-                    'message': '绑定信息失败'
+                    'status_code': 0,
+                    'message': '信息保存成功'
                 }
                 return res
-        try:
-            # 创建PhoneNumber对象
-            # phoneNumbers = phone_numbers.split(',')
-            # 对比数据库里的和传进来的电话号码，数据库里多余的要删除，参数里面有不一样的要添加
-            # oldNumbers = qrCode.phone_number # 注意在flask中使用一对多关系，获取到的是一个对象集合，需要获取到具体的值，才能用来做数值比较，否则就是对象之间作比较了，则永远捕不会相等，所以采用下面的写法
-            qrCode = QrCode.query.filter_by(qr_code_id=qrcodeid).first()  # 这重新查询一次
-            oldNumbers = [p.phone_number for p in qrCode.phone_number.all()]
-            delete, add = CommonUtils.compareArrays(oldNumbers, phone_numbers)
-            with open('flask.log', 'a+') as f:
-                f.write('\ndelete and add phonenumbers :%s,%s' % (delete,add))
-                f.close()
-            for d in delete:
-                p1=PhoneNumber.query.filter_by(phone_number=d, q_id=qrcodeid).first()
-                p1.delete()
-            for a in add:
-                p2=PhoneNumber(phone_number=a, q_id=qrcodeid)
-                p2.save()
-            res = {
-                'status_code': 0,
-                'message': '信息保存成功'
-            }
-            return res
+            except Exception as e:
+                print("创建PhoneNumber失败:", e)
+                res = {
+                    'status_code': -1,
+                    'message': '信息保存失败'
+                }
+                return res
         except Exception as e:
-            print("创建PhoneNumber失败:", e)
-            res = {
-                'status_code': -1,
-                'message': '信息保存失败'
-            }
-            return res
+            with open('flask.log', 'a+') as f:
+                f.write('\nfinal  exception :%s' % (e))
+                f.close()
