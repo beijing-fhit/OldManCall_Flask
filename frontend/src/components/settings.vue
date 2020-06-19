cf68b83c65f50799daffa59247a855bb587cb983
licaiMaker <841381242@qq.com>
2020-06-19 17:16:06 +0800

    update

--- frontend/src/components/settings.vue
+++ frontend/src/components/settings.vue
@@ -94,11 +94,11 @@
         </el-select>
       </el-row>
     <div class="info-fill-panel leftpadding ">
-      <el-row class="info-item border-bottom">
+      <el-row class="info-item-height-wrap-content border-bottom">
         <span class="long-info-name">正在吃的药</span>
         <el-input v-model="manInfo.drugs" maxlength=30 type="textarea" autosize class="long-info-content normal-input-style no-border-input" placeholder="预设内容" clearable></el-input>
       </el-row>
-      <el-row class="info-item">
+      <el-row class="info-item-height-wrap-content">
         <span class="long-info-name">正在进行的治疗</span>
         <el-input v-model="manInfo.treatment" maxlength=30 type="textarea" autosize class="long-info-content normal-input-style no-border-input" placeholder="预设内容" clearable></el-input>
       </el-row>
