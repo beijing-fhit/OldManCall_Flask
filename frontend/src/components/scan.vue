ce5b0d5ace43069a8e164ab6a72217bad781bc05
licaiMaker <841381242@qq.com>
2020-07-01 16:38:40 +0800

    update

--- frontend/src/components/scan.vue
+++ frontend/src/components/scan.vue
@@ -17,6 +17,7 @@ import wx from 'weixin-js-sdk'
 import api from '../api'
 export default {
   name: 'scan',
+  inject: ['reload'],
   components: {topbar},
   data () {
     return {
@@ -66,20 +67,21 @@ export default {
       }
     },
     startScan: function () {
-      var that = this
-      var ucallFreeId = sessionStorage.getItem('UcallFreeId')
-      if (ucallFreeId === null || ucallFreeId === '' || ucallFreeId === 'undefined' || ucallFreeId === undefined) {
-        return
-      }
-      wx.scanQRCode({
-        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-        scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
-        success: function (res) {
-          var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
-          that.handleScanResult(result)
-          console.log('scanQrCode:', result)
-        }
-      })
+      this.reload()
+      // var that = this
+      // var ucallFreeId = sessionStorage.getItem('UcallFreeId')
+      // if (ucallFreeId === null || ucallFreeId === '' || ucallFreeId === 'undefined' || ucallFreeId === undefined) {
+      //   return
+      // }
+      // wx.scanQRCode({
+      //   needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+      //   scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
+      //   success: function (res) {
+      //     var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
+      //     that.handleScanResult(result)
+      //     console.log('scanQrCode:', result)
+      //   }
+      // })
     },
     handleScanResult: function (content) {
       var that = this
